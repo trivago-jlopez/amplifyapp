3ac8aff489e35b884359aacd81391c317c4b3622
Javier Lopez <javier.lopez@trivago.com>
2019-07-30 11:29:40 +0200

    fixed cors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-const API_URL = 'https://samples.openweathermap.org/data/2.5/weather';
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather';
 
 class App extends Component {
     state = {
@@ -10,7 +10,7 @@ class App extends Component {
         const url = `${API_URL}?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22/`;
         axios.get(url, {headers: {'Access-Control-Allow-Origin': '*'}}).then(response => response.data)
         .then((data) => {
-          this.setState({ weathers: data })
+          this.setState({ weathers: data.weather })
           console.log(this.state.weathers)
          })
       }
@@ -22,9 +22,9 @@ class App extends Component {
         {this.state.weathers.map((weather) => (
           <div className="card">
            <div className="card-body">
-                <h5 className="card-title">{weather.name}</h5>
+                <h5 className="card-title">{weather.main}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">
-                    {weather.weather[0].description}
+                    {weather.description}
                 </h6>
             </div>
           </div>
